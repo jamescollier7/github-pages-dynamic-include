adff63b40ff5ac780cdb4895df8a7ff8792d6aae
James Collier <jamescollier7@users.noreply.github.com>
2023-05-03 16:48:46 -0600

    Update script.js

--- src/script.js
+++ src/script.js
@@ -2,12 +2,21 @@
   'use strict';
   
   const dynamicIncludeElements = document.querySelectorAll(`[data-dynamic-include]`)
+  const dynamicResults = new Map();
   dynamicIncludeElements.forEach((dynamicIncludeElement)=>{
     const dynamicIncludePath = dynamicIncludeElement.getAttribute(`data-dynamic-include`)
+    let result = ``
     
-    fetch(dynamicIncludePath).then((response) => response.text())
-    .then((html) => {
-      dynamicIncludeElement.outerHTML = html
-    }).catch((err) => console.warn('Something went wrong...', err))
+    if (dynamicResults.has(dynamicIncludePath)) {
+      result = dynamicResults.get(dynamicIncludePath)
+    } else {
+      result = await fetch(dynamicIncludePath).then((response) => response.text())
+      .then((html) => {
+        dynamicResults.set(dynamicIncludePath, html)
+        return html
+      }).catch((err) => console.warn(`Something went wrong...`, err))
+    }
+    
+    dynamicIncludeElement.outerHTML = result
   })
 })()
