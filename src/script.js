e91547c8a3b2d453747b368e70d70d15d8ee31e5
James Collier <jamescollier7@users.noreply.github.com>
2023-05-03 16:29:12 -0600

    Update script.js

--- src/script.js
+++ src/script.js
@@ -3,6 +3,11 @@
   
   const dynamicIncludeElements = document.querySelectorAll(`[data-dynamic-include]`)
   dynamicIncludeElements.forEach((dynamicIncludeElement)=>{
-    console.log(dynamicIncludeElement)
+    const dynamicIncludePath = dynamicIncludeElement.getAttribute(`data-dynamic-include`)
+    
+    fetch('dynamicIncludePath').then((response) => response.text())
+    .then((html) => {
+      dynamicIncludeElement.outerHTML = html
+    }).catch((err) => console.warn('Something went wrong...', err))
   })
 })()
